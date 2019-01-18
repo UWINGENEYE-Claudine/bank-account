0a5d545f8f0894d111e9ed4376d6f71e8608e449
UWINGENEYE-Claudine <cuwi2020@gmail.com>
2019-01-18 18:44:53 +0200

    create function

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+
+function BankAccount(name, initialDeposit, balance) {
+    this.userName = name;
+    this.firstDeposit = initialDeposit;
+    this.balance = balance;
+  }
+  
+  //method for adding a deposit.
+  BankAccount.prototype.makeDeposit = function(deposit) {
+    this.balance += deposit;
+  }
+  //method for substracting a withdrawl.
+  BankAccount.prototype.makeWithdrawl = function(withdrawl) {
+    this.balance -= withdrawl;
+  }
+  
+  
+  
+  //User interface logic
+  
+  $(document).ready(function(){
+    $('form#submit_new_account').submit(function(event) {
+      event.preventDefault();
+       name = $('#user_name').val();
+       initialDeposit = parseInt($('#initial_deposit').val());
+       newBalance = initialDeposit;
+       userAccount = new BankAccount(name, initialDeposit, newBalance);
+      console.log(userAccount);
+      $('.new_account').remove();
+      $('.balance_here').text(`$${userAccount.balance}`);
+  
+        $('#make_deposit').submit(function(event) {
+          event.preventDefault();
+           deposit = parseInt($('#current_deposit').val());
+          userAccount.makeDeposit(deposit);
+          $('.balance_here').text(`$${userAccount.balance}`);
+          $('#make_deposit').text('reset');
+        });
+  
+        $('#make_withdrawl').submit(function(event) {
+          event.preventDefault();
+           withdrawl = parseInt($('#current_withdrawl').val());
+          userAccount.makeWithdrawl(withdrawl);
+          $('.balance_here').text(`$${userAccount.balance}`);
+          $('#make_withdrawl').trigger('reset');
+        })
+    })
+  })
+  
